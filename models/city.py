b966dbd135dbde7e9c745a80b3b8b16bf7cb05a0
Daniela1026 <cartagenad49@gmail.com>
2022-12-02 13:03:23 -0500

    update table cities

--- models/city.py
+++ models/city.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 import sqlalchemy
+import models
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
+from os import environ
 
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", cascade="all, delete", backref="cities")
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    else:
+        name = ''
+        state_id = ''
