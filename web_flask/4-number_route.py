664914c712aee59f94fd03401ed09226d93ba3c6
Daniela1026 <cartagenad49@gmail.com>
2022-12-07 11:11:56 -0500

    display n is a number only if n is an integer

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 """
-display “n is a number” only if n is an integer
+followed by the value of the text variable
 """
-
-from markupsafe import escape
-
 from flask import Flask
-
 app = Flask(__name__)
 
 
@@ -42,10 +38,8 @@ def python(text='is cool'):
 
 @app.route("/number/<int:n>", strict_slashes=False)
 def vernumber(n):
-    """
-    Display “n is a number” only if n is an integer.
-    """
     return '{} is a number'.format(escape(n))
 
-    if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
