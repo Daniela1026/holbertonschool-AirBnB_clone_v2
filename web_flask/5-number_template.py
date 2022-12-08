2bff7782293e4bd262f890f679c43fa97abff87a
Daniela1026 <cartagenad49@gmail.com>
2022-12-08 09:53:37 -0500

    update

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -44,7 +44,7 @@ def vernumber(n):
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
     """ Display a HTML page only if n is an integer """
-    return render_template("5-number.html", n=n)
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == "__main__":
