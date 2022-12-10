01f74ea8df39de25eee3f2338cf1a811a9a0324b
Daniela1026 <cartagenad49@gmail.com>
2022-12-10 09:55:39 -0500

    update

--- models/user.py
+++ models/user.py
@@ -1,34 +1,17 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship, backref
-import os
-
-HBNB_TYPE_STORAGE = os.getenv('HBNB_TYPE_STORAGE')
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class User(BaseModel, Base if HBNB_TYPE_STORAGE == 'db' else object):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    if HBNB_TYPE_STORAGE == "db":
-        __tablename__ = 'users'
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128))
-        last_name = Column(String(128))
-        places = relationship(
-            "Place",
-            cascade="all,delete",
-            backref=backref("user", cascade="all,delete"),
-            passive_deletes=True,
-            single_parent=True
-        )
-        reviews = relationship("Review", cascade="all,delete",
-                               backref=backref("user", cascade="all,delete"),
-                               passive_deletes=True,
-                               single_parent=True)
-    else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
+    __tablename__ = "users"
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship("Place", backref="user",
+                          cascade="all, delete, delete-orphan")
