45afc976fb0fa7c7b32a3ef4ea06c7c9327d6888
Daniela1026 <cartagenad49@gmail.com>
2022-12-10 20:38:53 -0500

    update

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
 """This defines the DBStorage class"""
 
-from os import getenv
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker, scoped_session
-from sqlalchemy import create_engine
-from models.base_model import BaseModel
-from models.base_model import Base
+import os
+from models.base_model import BaseModel, Base
+from models.amenity import Amenity
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-from models.amenity import Amenity
-
 
 
 user = os.getenv('HBNB_MYSQL_USER')
@@ -20,58 +18,63 @@ password = os.getenv('HBNB_MYSQL_PWD')
 host = os.getenv('HBNB_MYSQL_HOST')
 database = os.getenv('HBNB_MYSQL_DB')
 
-classes = {'User': User,
-           'State': State,
-           'City': City,
-           'Amenity': Amenity,
-           'Place': Place,
-           'Review': Review}
+classes = {
+    'City': City,
+    'User': User,
+    'Review': Review,
+    'State': State,
+    'Place': Place,
+    'Amenity': Amenity
+    }
 
 
 class DBStorage:
     __engine = None
     __session = None
 
-    self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
-                                      format(getenv('HBNB_MYSQL_USER'),
-                                             getenv('HBNB_MYSQL_HOST'),
-                                             getenv('HBNB_MYSQL_DB'),
-                                             getenv('HBNB_MYSQL_PWD')),
-                                      pool_pre_ping=True)
-    if getenv('HBNB_ENV') == "test":
-            Base.metadata.drop_all(self.engine)
+    def __init__(self):
+        self.__engine = create_engine(
+            "mysql+mysqldb://{}:{}@{}/{}".format(
+                user, password, host, database
+                ),
+            pool_pre_ping=True
+            )
+
+        if os.getenv("HBNB_ENV ") == 'test':
+            meta = MetaData(self.__engine)
+            meta.drop_all()
 
     def all(self, cls=None):
-        if cls is None:
-            objs = self.__session.query(State).all()
-            objs.extend(self.__session.query(City).all())
-            objs.extend(self.__session.query(User).all())
-            objs.extend(self.__session.query(Place).all())
-            objs.extend(self.__session.query(Review).all())
-            objs.extend(self.__session.query(Amenity).all())
-
+        dic = {}
+        if not cls:
+            for actual_class in classes.values():
+                object_list = self.__session.query(actual_class).all()
+                for obj in object_list:
+                    key = "{}.{}".format(type(obj).__name__, obj.id)
+                    dic[key] = obj
         else:
-            if type(cls) == str:
-                cls = eval(cls)
-            objs = self.__session.query(cls)
-        return {"{}.{}".format(type(o).__name__, o.id): o for o in objs}
+            object_list = self.__session.query(cls).all()
+            for obj in object_list:
+                key = "{}.{}".format(type(obj).__name__, obj.id)
+                dic[key] = obj
+        return dic
 
     def new(self, obj):
-        if obj:
-            self.__session.add(obj)
+        self.__session.add(obj)
 
     def save(self):
         self.__session.commit()
 
     def delete(self, obj=None):
-        if obj is not None:
+        if obj:
             self.__session.delete(obj)
 
-    def close(self):
-        Session.close(self.__session)
-            
     def reload(self):
         Base.metadata.create_all(self.__engine)
-        sess = sessionmaker(expire_on_commit=False, bind=self.__engine)
-        Session1 = scoped_session(sess)
-        self.__session = Session1()
+        Session = sessionmaker(expire_on_commit=False, bind=self.__engine)
+        Scoped = scoped_session(Session)
+        self.__session = Scoped
+
+    def close(self):
+        """remove method on private session attribute"""
+        self.__session.remove()
