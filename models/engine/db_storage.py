1e72a8218e91061dd5c531cd21aade643459c8a0
Daniela1026 <cartagenad49@gmail.com>
2022-12-10 20:37:05 -0500

    update

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """This defines the DBStorage class"""
 
-from sqlalchemy import create_engine, MetaData
+from os import getenv
 from sqlalchemy.orm import sessionmaker, scoped_session
-import os
-from models.base_model import BaseModel, Base
-from models.amenity import Amenity
+from sqlalchemy import create_engine
+from models.base_model import BaseModel
+from models.base_model import Base
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-from sqlalchemy.orm import Session
+from models.amenity import Amenity
 
 
 
@@ -32,37 +32,39 @@ class DBStorage:
     __engine = None
     __session = None
 
-    def __init__(self):
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-                                      .format(user, password, host, database),
+    self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
+                                      format(getenv('HBNB_MYSQL_USER'),
+                                             getenv('HBNB_MYSQL_HOST'),
+                                             getenv('HBNB_MYSQL_DB'),
+                                             getenv('HBNB_MYSQL_PWD')),
                                       pool_pre_ping=True)
-        if os.getenv("HBNB_ENV ") == 'test':
-            meta = MetaData(self.__engine)
-            meta.drop_all()
+    if getenv('HBNB_ENV') == "test":
+            Base.metadata.drop_all(self.engine)
 
     def all(self, cls=None):
-        dic = {}
-        if not cls:
-            for actual_class in classes.values():
-                object_list = self.__session.query(actual_class).all()
-                for obj in object_list:
-                    key = "{}.{}".format(type(obj).__name__, obj.id)
-                    dic[key] = obj
+        if cls is None:
+            objs = self.__session.query(State).all()
+            objs.extend(self.__session.query(City).all())
+            objs.extend(self.__session.query(User).all())
+            objs.extend(self.__session.query(Place).all())
+            objs.extend(self.__session.query(Review).all())
+            objs.extend(self.__session.query(Amenity).all())
+
         else:
-            object_list = self.__session.query(cls).all()
-            for obj in object_list:
-                key = "{}.{}".format(type(obj).__name__, obj.id)
-                dic[key] = obj
-        return dic
+            if type(cls) == str:
+                cls = eval(cls)
+            objs = self.__session.query(cls)
+        return {"{}.{}".format(type(o).__name__, o.id): o for o in objs}
 
     def new(self, obj):
-        self.__session.add(obj)
+        if obj:
+            self.__session.add(obj)
 
     def save(self):
         self.__session.commit()
 
     def delete(self, obj=None):
-        if obj:
+        if obj is not None:
             self.__session.delete(obj)
 
     def close(self):
