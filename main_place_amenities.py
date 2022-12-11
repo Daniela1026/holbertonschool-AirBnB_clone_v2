89c20c40fc6de774c77f31292c0613e53a1dc036
Daniela1026 <cartagenad49@gmail.com>
2022-12-11 02:28:27 -0500

    update

--- main_place_amenities.py
+++ main_place_amenities.py
@@ -1,21 +1,27 @@
 #!/usr/bin/python3
-""" Place - Amenity"""
+""" Test link Many-To-Many Place <> Amenity
+"""
 from models import *
 
+# creation of a State
 state = State(name="California")
 state.save()
 
+# creation of a City
 city = City(state_id=state.id, name="San Francisco")
 city.save()
 
-user = User(email="johndoe@gmail.com", password="johnpwd")
+# creation of a User
+user = User(email="john@snow.com", password="johnpwd")
 user.save()
 
+# creation of 2 Places
 place_1 = Place(user_id=user.id, city_id=city.id, name="House 1")
 place_1.save()
 place_2 = Place(user_id=user.id, city_id=city.id, name="House 2")
 place_2.save()
 
+# creation of 3 various Amenity
 amenity_1 = Amenity(name="Wifi")
 amenity_1.save()
 amenity_2 = Amenity(name="Cable")
@@ -23,9 +29,11 @@ amenity_2.save()
 amenity_3 = Amenity(name="Oven")
 amenity_3.save()
 
+# link place_1 with 2 amenities
 place_1.amenities.append(amenity_1)
 place_1.amenities.append(amenity_2)
 
+# link place_2 with 3 amenities
 place_2.amenities.append(amenity_1)
 place_2.amenities.append(amenity_2)
 place_2.amenities.append(amenity_3)
